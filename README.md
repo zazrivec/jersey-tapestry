092493f3484329b79ee7b256ddf4d1d72f1f7e6e
zazrivec <jan.zazrivec@gmail.com>
2013-04-15 22:28:24 +0200

    doc improvement, JsonProvider

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ Tapestry5 support is enabled by referencing the class TapestryContainer in the w
         <servlet-class>com.sun.jersey.spi.tapestry.container.servlet.TapestryContainer</servlet-class>
         <init-param>
             <param-name>javax.ws.rs.Application</param-name>
-            <param-value>com.example.application.Application</param-value>
+            <param-value>com.example.application.MyApplication</param-value>
         </init-param>
         <load-on-startup>1</load-on-startup>
     </servlet>
@@ -30,4 +30,61 @@ Tapestry5 support is enabled by referencing the class TapestryContainer in the w
         <url-pattern>/api/v1/*</url-pattern>
     </servlet-mapping>
    </web-app>
+```
+
+Example JAX-RS Application class:
+
+```java
+    public class MyApplication extends Application {
+
+        @Override
+        public Set<Class<?>> getClasses() {
+            return new HashSet<Class<?>>(Arrays.asList(TapestryJsonProvider.class, GuideResource.class));
+        }
+
+    }
+```
+
+Example JAX-RS Resource class:
+
+```java
+    @Path("/guide")
+    public class GuideResource {
+
+        // we can inject tapestry service into jax-rs resource
+        @Inject
+        private ProgramMetadataService programMetadataService;
+
+        @Inject
+        private ProjectService projectService;
+
+        // we can process tapestry JSONObject class as result and input parameter
+        @GET
+        @Produces(MediaType.APPLICATION_JSON)
+        @Path("/review/{projectId}")
+        public JSONObject getReviewGuide(@PathParam("projectId") String projectId) {
+            Project project = projectService.getProject(new ObjectId(projectId));
+            return programMetadataService.getGuide(project.getProgram().getReviewDefinition().getGuideSections());
+        }
+
+        // and of course, we can make all the jax-rs apu things
+        @GET
+        @Produces(MediaType.APPLICATION_JSON)
+        @Path("/reviewerReview/{projectId}")
+        public Response getProjectReview(@PathParam("projectId") String projectId) {
+            try {
+                Project project = projectService.getProject(new ObjectId(projectId));
+                ReviewDefinition definition = project.getProgram().getReviewDefinition();
+                ReviewPart reviewPart = ModelUtils.toClient(reviewService.getReviewPartForLoggedInUser(project), definition);
+                ReviewerMetadataAndData result = new ReviewerMetadataAndData(project, EnumUtils.create(ProjectReviewRating.class), definition,
+                        reviewPart);
+
+                return Response.ok(result).build();
+            }
+            catch (IllegalAccessException e) {
+                return Response.status(Status.FORBIDDEN).build();
+            }
+        }
+
+    }
 ```
---
+++ src/main/java/com/sun/jersey/spi/tapestry/container/TapestryJsonProvider.java
@@ -0,0 +1,102 @@
+package com.sun.jersey.spi.tapestry.container;
+
+import com.sun.jersey.core.util.ReaderWriter;
+import org.apache.tapestry5.json.JSONObject;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+import javax.ws.rs.ext.Providers;
+import java.io.*;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.nio.charset.Charset;
+import java.util.Map;
+
+@Provider
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class TapestryJsonProvider implements MessageBodyReader<JSONObject>, MessageBodyWriter<JSONObject> {
+
+    private static final String CHARSET = "charset";
+
+    public static final Charset UTF8 = Charset.forName("UTF-8");
+
+    @Context
+    protected Providers providers;
+
+    @Override
+    public boolean isReadable(Class<?> type, Type genericType,
+                              Annotation[] annotations, MediaType mediaType) {
+        return JSONObject.class == getDomainClass(genericType);
+    }
+
+    String readInputStreamAsString(Reader entityReader)
+            throws IOException {
+        ByteArrayOutputStream buf = new ByteArrayOutputStream();
+        int result = entityReader.read();
+        while (result != -1) {
+            byte b = (byte) result;
+            buf.write(b);
+            result = entityReader.read();
+        }
+        return buf.toString();
+    }
+
+    public static final Charset getCharset(MediaType m) {
+        String name = (m == null) ? null : m.getParameters().get("charset");
+        return (name == null) ? UTF8 : Charset.forName(name);
+    }
+
+    @Override
+    public JSONObject readFrom(Class<JSONObject> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, String> httpHeaders, InputStream entityStream) throws IOException, WebApplicationException {
+        try {
+            Charset charset = getCharset(mediaType);
+            Map<String, String> mediaTypeParameters = mediaType.getParameters();
+            Reader entityReader = new InputStreamReader(entityStream, charset);
+            return new JSONObject(readInputStreamAsString(entityReader));
+        } catch (Exception e) {
+            throw new WebApplicationException(e);
+        }
+    }
+
+    @Override
+    public void writeTo(JSONObject jsonObject, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+        try {
+            Charset charset = getCharset(mediaType);
+            OutputStreamWriter osw = new OutputStreamWriter(entityStream, charset);
+            PrintWriter pw = new PrintWriter(osw);
+            jsonObject.print(pw);
+            pw.flush();
+        } catch (Exception e) {
+            throw new WebApplicationException(e);
+        }
+    }
+
+    @Override
+    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return isReadable(type, genericType, annotations, mediaType);
+    }
+
+    @Override
+    public long getSize(JSONObject jsonObject, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return -1;
+    }
+
+    private Class<?> getDomainClass(Type genericType) {
+        if (genericType instanceof Class) {
+            return (Class<?>) genericType;
+        } else if (genericType instanceof ParameterizedType) {
+            return (Class<?>) ((ParameterizedType) genericType).getActualTypeArguments()[0];
+        } else {
+            return null;
+        }
+    }
+}
