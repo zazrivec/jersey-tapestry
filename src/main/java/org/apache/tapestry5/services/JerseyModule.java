640842dd7c859b196885f65cb95e171d0355abd4
zazrivec <jan.zazrivec@gmail.com>
2014-02-22 10:28:11 +0100

    module code clean up

--- src/main/java/org/apache/tapestry5/services/JerseyModule.java
+++ src/main/java/org/apache/tapestry5/services/JerseyModule.java
@@ -2,27 +2,20 @@ package org.apache.tapestry5.services;
 
 import org.apache.tapestry5.ioc.OrderedConfiguration;
 import org.apache.tapestry5.ioc.ServiceBinder;
-import org.apache.tapestry5.ioc.annotations.InjectService;
+import org.apache.tapestry5.ioc.annotations.Contribute;
+import org.apache.tapestry5.ioc.annotations.Local;
 
 public class JerseyModule {
 
-
-    private final RequestGlobals requestGlobals;
-
-    public JerseyModule(RequestGlobals requestGlobals)
-    {
-        this.requestGlobals = requestGlobals;
-    }
 
     public static void bind(ServiceBinder binder)
     {
         binder.bind(HttpServletRequestFilter.class, RestApiRequestFilter.class).withSimpleId();
     }
 
-    public void contributeHttpServletRequestHandler(OrderedConfiguration<HttpServletRequestFilter> configuration,
-
-                                                    @InjectService("RestApiRequestFilter")
-                                                    HttpServletRequestFilter restApiRequestFilter)
+    @Contribute(HttpServletRequestHandler.class)
+    public void contributeHttpServletRequestHandler(
+            OrderedConfiguration<HttpServletRequestFilter> configuration, @Local HttpServletRequestFilter restApiRequestFilter)
     {
         configuration.add("RestApiStoreIntoGlobals", restApiRequestFilter, "before:IgnoredPaths");
     }
